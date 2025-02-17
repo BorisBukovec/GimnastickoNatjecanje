63d5ead5fbb08eedc5e291cd6d9e5a05459b8000
Boris Bukovec <botaosijek@gmail.com>
2025-02-17 18:51:00 +0100

    priprema produkcija

--- Backend/Baza.sql
+++ Backend/Baza.sql
@@ -1,16 +1,16 @@
 
 
-use master;
-go
-
-drop database if exists gimnastika;
-go
-
-create database gimnastika collate Croatian_CI_AS;
-go
-
-use gimnastika;
-go
+SELECT name, collation_name FROM sys.databases;
+GO
+ALTER DATABASE db_ab2ebf_gimnastika SET SINGLE_USER WITH
+ROLLBACK IMMEDIATE;
+GO
+ALTER DATABASE db_ab2ebf_gimnastika COLLATE Croatian_CI_AS;
+GO
+ALTER DATABASE db_ab2ebf_gimnastika SET MULTI_USER;
+GO
+SELECT name, collation_name FROM sys.databases;
+GO
 
 create table klubovi(
 sifra int not null primary key identity(1,1),
